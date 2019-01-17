76d67def36bddf3abde81cfa231ff43f0dbfb365
Jen-Agyemang <jenagyemang@outlook.com>
2019-01-17 12:38:34 -0800

    add StringAndList
    StringAndList

---
+++ DailyReports/ArrayAndList/ArrayAndList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //  IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+            //  1.Create an array of strings. Ask the user to select an index of the Array and then display the string at that index on the screen.
+
+            //2.Create an array of integers. Ask the user to select an index of the Array and then display the integer at that index on the screen.
+
+            //3.Add in a message that displays when the user selects an index that doesn’t exist.
+
+            //4.Create a List of strings. Ask the user to select an index of the List and then display the content at that index on the screen.
+
+
+
+        }
+    }
+}
