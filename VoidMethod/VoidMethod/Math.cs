3078e9a79e5b5a5cbf7adf177283dbeac46b2255
Jen-Agyemang <jenagyemang@outlook.com>
2019-01-29 14:03:16 -0800

    add void method
    void method

---
+++ VoidMethod/VoidMethod/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+   public class Math
+    {
+       public void Operation(int a, int b)
+        {
+            a = 2;
+            b = 5;
+        }
+           
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Math calc = new Math();
+            int myValue = 5;
+            int secondValue = 2;
+            calc.Operation(myValue, secondValue);
+            Console.WriteLine(myValue + secondValue);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
