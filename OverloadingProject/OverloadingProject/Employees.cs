d196451f3f5b5efe5005fc821020c583f84310b0
Jen-Agyemang <jenagyemang@outlook.com>
2019-02-06 12:20:03 -0800

    Add OverloadingProject

---
+++ OverloadingProject/OverloadingProject/Employees.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingProject
+{
+    public class Employees
+    {
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+
+        public static bool operator == (Employees employee1, Employees employee2)
+            
+        {
+            return employee1.Id == employee2.Id;
+
+        }
+        public static bool operator !=(Employees employee1, Employees employee2)
+
+        {
+            return employee1.Id != employee2.Id;
+
+        }
+        
+
+    }
+}
---
+++ OverloadingProject/OverloadingProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees employee1 = new Employees();
+            Employees employee2 = new Employees();
+            employee1.Firstname = "Nana";
+            employee1.Lastname = "Jen";
+            employee2.Firstname = "Nana";
+            employee2.Lastname = "Jen";
+            employee1.Id = "93982389";
+            employee2.Id = "32891332";
+            Console.WriteLine("Does employee1 and employee2 have the same firstnames? " + (employee1.Firstname == employee2.Firstname));
+            Console.WriteLine("Does employee1 and employee2 have same lastnames? " + (employee1.Lastname == employee2.Lastname));
+            Console.WriteLine("Does employee1 and employee2 have the same Id? " + (employee1.Id == employee2.Id));
+            Console.ReadLine();
+
+        }
+    }
+}
