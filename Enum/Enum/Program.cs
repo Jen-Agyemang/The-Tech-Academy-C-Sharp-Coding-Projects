f64bce64f55c5a954c35165efb962eb241c4a2ad
Jen-Agyemang <jenagyemang@outlook.com>
2019-02-07 14:07:04 -0800

    Add Enum

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TheDaysOfTheWeek theDays = new TheDaysOfTheWeek();
+                theDays.TheDay = theDays.TheDay;
+                Console.WriteLine("Please select the current day of the week?");
+                string daySelected = Console.ReadLine();
+                var week = System.Enum.Parse(typeof(Days), daySelected);
+                Console.WriteLine(System.Enum.Parse(typeof(Days), daySelected));
+                //Console.WriteLine("Today is : ", System.Enum.Parse(typeof(Days), daySelected));
+                Console.WriteLine("Today is {0}. ", System.Enum.Parse(typeof(Days), daySelected));
+              //  Console.WriteLine(System.Enum.GetName(typeof(Days), theDays));
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Unable to convert to a date. Please enter an actual day of the week. Thank you.");
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+           // DateTime d = DateTime.Now;
+        }
+    }
+}
---
+++ Enum/Enum/TheDaysOfTheWeek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    public class TheDaysOfTheWeek
+    {
+        public Days TheDay { get; set; }
+    }
+        public enum Days
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Satuday
+        }
+    
+    
+}
