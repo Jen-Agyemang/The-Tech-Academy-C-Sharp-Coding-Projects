256ac880ce7dcafa442e061391e03f9a0e2e789a
Jen-Agyemang <jenagyemang@outlook.com>
2019-02-23 16:51:06 -0800

    add CarInsurance

---
+++ TheMVCWebApplication1/TheMVCWebApplication1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheMVCWebApplication1.MvcApplication" Language="C#" %>
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Casino.TwentyOne;
 using System.IO;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace TwentyOne
 {
@@ -16,55 +18,142 @@ namespace TwentyOne
             const string casinoName = "Grand Hotel and Casino";
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();
+                {
 
-            bool validAnswer = false;
-            int bank = 0;
-            while (!validAnswer)
-            {
-                Console.WriteLine("And how much money did you bring today?");
-                validAnswer = int.TryParse(Console.ReadLine(), out bank);
-                if (!validAnswer) Console.WriteLine("Please, enter digits only, no decimals. For example(23.432) is not allowed. Thank you.");
-            }
-           
-            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
-            string answer = Console.ReadLine().ToLower();
-            if (answer == "yes" || answer == "yeah" || answer == "yup")
-            {
-                Player player = new Player(playerName, bank);
-                player.Id = Guid.NewGuid();
-                using (StreamWriter file = new StreamWriter(@"C:\Users\Owner\Desktop\FilePath.txt", true))
+                    foreach (var exception in Exceptions)
+                    {
+                        Console.Write(exception.Id + " ");
+                        Console.Write(exception.ExceptionType + " ");
+                        Console.Write(exception.TimeStamp + " ");
+                        Console.WriteLine();
+
+                    }
+                    Console.Read();
                     
+                }
 
-                {
-                    file.WriteLine(player.Id);
+                    bool validAnswer = false;
+                    int bank = 0;
+                    while (!validAnswer)
+                    {
+                        Console.WriteLine("And how much money did you bring today?");
+                        validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                        if (!validAnswer) Console.WriteLine("Please, enter digits only, no decimals. For example(23.432) is not allowed. Thank you.");
+                    }
+
+                    Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+                    string answer = Console.ReadLine().ToLower();
+                    if (answer == "yes" || answer == "yeah" || answer == "yup")
+                    {
+                        Player player = new Player(playerName, bank);
+                        player.Id = Guid.NewGuid();
+                        using (StreamWriter file = new StreamWriter(@"C:\Users\Owner\Desktop\FilePath.txt", true))
+
+
+                        {
+                            file.WriteLine(player.Id);
+                        }
+                        Game game = new TwentyOneGame();
+                        game += player;
+                        player.IsActivelyPlaying = true;
+                        while (player.IsActivelyPlaying && player.Balance > 0)
+                        {
+                            try
+                            {
+                                game.Play();
+                            }
+                            catch (FraudException ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                                UpdateDbWithException(ex);
+                                Console.ReadLine();
+                                return;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("An error occured, please, contact your system administrator.");
+                                UpdateDbWithException(ex);
+                                Console.ReadLine();
+                                return;
+                            }
+                        }
+                        game -= player;
+                        Console.WriteLine("Thank you for playing!");
+                    }
+                    Console.WriteLine("Feel free to look around the casino. Bye for now.");
+                    Console.Read();
                 }
-                Game game = new TwentyOneGame();
-                game += player;
-                player.IsActivelyPlaying = true;
-                while (player.IsActivelyPlaying && player.Balance > 0)
+
+                private static void UpdateDbWithException(Exception ex)
                 {
-                    try
+                    string connectionString = @"Data Source = (localdb)\ProjectsV13;
+                                       Initial Catalog = TwentyOneGame; 
+                                        Integrated Security = True; 
+                                        Connect Timeout = 30;
+                                        Encrypt = False; 
+                                        TrustServerCertificate = False; 
+                                        ApplicationIntent = ReadWrite; 
+                                        MultiSubnetFailover = False";
+
+                    string queryString = @"INSERT INTO EXCEPTIONS (ExceptionType, ExceptionMessage, TimeStamp) VALUES (@ExceptionType, @ExceptionMessage, @TimeStamp";
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
                     {
-                        game.Play();
-                    }
-                    catch (FraudException)
-                    {
-                        Console.WriteLine("Security! Kick this person out.");
-                        Console.ReadLine();
-                        return;
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("An error occured, please, contact your system administrator.");
-                        Console.ReadLine();
-                        return;
+                        SqlCommand command = new SqlCommand(queryString, connection);
+                        command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);
+                        command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                        command.Parameters.Add("@TimeStamp", SqlDbType.DateTime);
+
+                        command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();
+                        command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                        command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
                     }
                 }
-                game -= player;
-                Console.WriteLine("Thank you for playing!");
+             private static List<ExceptionEntity> ReadExceptions()
+            {
+                string connectionString = @"Data Source = (localdb)\ProjectsV13;
+                                       Initial Catalog = TwentyOneGame; 
+                                        Integrated Security = True; 
+                                        Connect Timeout = 30;
+                                        Encrypt = False; 
+                                        TrustServerCertificate = False; 
+                                        ApplicationIntent = ReadWrite; 
+                                        MultiSubnetFailover = False";
+
+                string queryString = @"SELECT Id, ExceptionType, ExceptionMessage, TimeStamp FROM Exceptions";
+
+                List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(queryString, connection);
+
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ExceptionEntity exception = new ExceptionEntity();
+                        exception.Id = Convert.ToInt32(reader["Id]"]);
+                        exception.ExceptionType = reader["ExceptionType"].ToString();
+                        exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                        exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                        Exceptions.Add(exception);
+                    }
+                    connection.Close();
+                }
+                return Exceptions;
+                    
+              }
             }
-            Console.WriteLine("Feel free to look around the casino. Bye for now.");
-            Console.Read();
         }
-    }
+    
 }
